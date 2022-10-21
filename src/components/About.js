813773889f0d3491e8cb5ee6df0ff0e638c1ec7b
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2022-10-21 13:53:00 +0100

    added about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,28 @@
 import "../styles/About.css";
+import abtImg from "../images/background.jpg";
 
 function About() {
    return (
       <>
-         <div className="about">About</div>
+         <div className="about">
+            <div className="aboutImg">
+               <img src={abtImg} alt="img" />
+            </div>
+            <div className="aboutText">
+               <h3>ABOUT US</h3>
+               <h1>We Provide Solutions Clients</h1>
+               <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Nostrum a voluptates numquam officiis deleniti praesentium
+                  voluptatum, debitis consequatur facere dicta ratione magnam
+                  repellendus voluptatem voluptatibus illum cum molestiae eum
+                  dignissimos. Lorem ipsum, dolor sit amet consectetur
+                  adipisicing elit. Nostrum a voluptates numquam officiis
+                  deleniti praesentium voluptatum, debitis consequatur facere
+               </p>
+               <button className="aboutBtn">Learn More</button>
+            </div>
+         </div>
       </>
    );
 }
